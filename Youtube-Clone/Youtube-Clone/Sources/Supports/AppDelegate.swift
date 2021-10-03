e8dc94bad18c4b847000cc3699252222030a137e
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-10-04 03:39:25 +0900

    ➕[ADD] CheckVC 화면 구현 완료(#2)

--- Youtube-Clone/Youtube-Clone/Sources/Supports/AppDelegate.swift
+++ Youtube-Clone/Youtube-Clone/Sources/Supports/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = SignupVC()
+        window.rootViewController = CheckVC()
         window.makeKeyAndVisible()
         window.backgroundColor = .white
         self.window = window
