3c6e806619ea1247358041157e85f8c58069059d
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-10-04 03:30:06 +0900

    ➕[ADD] 회원가입 뷰 완료(#2)

--- Youtube-Clone/Youtube-Clone/Sources/Supports/AppDelegate.swift
+++ Youtube-Clone/Youtube-Clone/Sources/Supports/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = LoginVC()
+        window.rootViewController = SignupVC()
         window.makeKeyAndVisible()
         window.backgroundColor = .white
         self.window = window
