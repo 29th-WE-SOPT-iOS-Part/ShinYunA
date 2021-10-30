39b31289a9e68298ba9d49a5535a5e2c1a83dae7
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-10-30 14:22:31 +0900

    ➕[ADD] 새로운 세미나 프로젝트 생성

---
+++ Seminar/fourthSeminar/fourthSeminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fourthSeminar
+//
+//  Created by SHIN YOON AH on 2021/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
